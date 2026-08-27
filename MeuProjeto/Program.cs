7d7871485079365cf6219845bb60382e55691077
Mattheus-Costa <costamattheus4@gmail.com>
2026-08-27 20:07:43 -0300

    Atualizações

--- MeuProjeto/Program.cs
+++ MeuProjeto/Program.cs
@@ -1,32 +1,90 @@
 ﻿
+using System.Collections;
 using MeuProjeto;
 
-Console.WriteLine("Digite o nome do livro: ");
-string nomeLivro = Console.ReadLine() ?? "";
+int opcao;
+List<Livro> livros = new List<Livro>();
 
-Console.WriteLine("Digite o nome do autor: ");
-string autorLivro = Console.ReadLine()  ?? "";
+do
+{
+    Console.WriteLine("===SISTEMA LIVRARIA===");
+    Console.WriteLine("1 - Cadastrar Livros");
+    Console.WriteLine("2 - Listar Livros");
+    Console.WriteLine("3 - Sair");
+    opcao =  int.Parse(Console.ReadLine()  ?? "");
 
-Console.WriteLine("Digite a nacionalidade do autor: ");
-string autorNacionalidade = Console.ReadLine()  ?? "";
+    switch (opcao)
+    {
+        case 1:
+           
+            Console.WriteLine("Digite o nome do livro: ");
+            string nomeLivro = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Digite o nome do autor: ");
+            string autorLivro = Console.ReadLine()  ?? "";
+
+            Console.WriteLine("Digite a nacionalidade do autor: ");
+            string autorNacionalidade = Console.ReadLine()  ?? "";
 
 
-Console.WriteLine("Digite o gênero do livro: ");
-string generoLivro = Console.ReadLine()  ?? "";
+            Console.WriteLine("Digite o gênero do livro: ");
+            string generoLivro = Console.ReadLine()  ?? "";
 
-Console.WriteLine("Digite o ano de lançamento do livro: ");
-int anoLivro = int.Parse(Console.ReadLine()  ?? "");
+            Console.WriteLine("Digite o ano de lançamento do livro: ");
+            int anoLivro = int.Parse(Console.ReadLine()  ?? "");
 
-Autor autor = new Autor(
-    autorLivro,
-    autorNacionalidade
-);
+            Autor autor = new Autor(
+                autorLivro,
+                autorNacionalidade
+            );
 
-Livro livro = new Livro (
-    nomeLivro,
-    anoLivro,
-    generoLivro,
-    autor
-);
+            Livro livro = new Livro (
+                nomeLivro,
+                anoLivro,
+                generoLivro,
+                autor
+            );
 
-livro.Apresentar();
+            livros.Add(livro);
+
+            break;
+
+         case 2:
+
+
+                if (livros.Count == 0)
+                {
+                    Console.WriteLine("Nenhum livro cadastrado.");
+                }
+                else
+                {
+                    int i = 1;
+                    foreach (Livro livroCadastrado in livros)
+                    {   
+                
+                    
+                            Console.WriteLine($"===LIVRO {i}===");
+                            livroCadastrado.Apresentar();
+
+                            i++;
+                }}
+
+            break;
+
+        
+        case 3:
+             Console.WriteLine("Encerrando...");
+             break;
+
+        default:
+
+            Console.WriteLine("Opção Inválida!");
+            break;
+    }
+
+       
+
+}while(opcao != 3);
+
+
+
--- MeuProjeto/obj/Debug/net10.0/MeuProjeto.AssemblyInfo.cs
+++ MeuProjeto/obj/Debug/net10.0/MeuProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b84f5b8847f941718816057c17040937bc4db296")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
